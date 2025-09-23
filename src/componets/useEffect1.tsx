de007d8a15dd12a61723a660c134fd231ec5e8d6
Beutife <biholuwatisefeh@gmail.com>
2025-09-24 00:24:15 +0100

    react hooks

---
+++ src/componets/useEffect1.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState,useEffect } from "react";
+
+
+const UseEffect = () => {
+   
+    const [userName, setUserName] = useState("")
+     useEffect (() => {
+        setTimeout(()=>{
+            setUserName("John");
+        }, 1500)
+     }, [])
+    return<>
+      <h4>{userName}</h4>
+      <input type="text"
+      value={userName}
+      onChange={(e)=> setUserName(e.target.value)}
+      />
+    </>
+
+}
+export default UseEffect
